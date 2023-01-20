0707dd85ce52c7778289d97a6523a3f6cdbf5c5d
Wen <jbw5872@psu.edu>
2023-01-20 09:30:13 -0500

    Lab2 complete (no extra credit)

--- src/lab2/cities_n_routes.py
+++ src/lab2/cities_n_routes.py
@@ -17,7 +17,39 @@ def get_randomly_spread_cities(size, n_cities):
     :param n_cities: The number of cities to generate
     :return: A list of cities with random x and y coordinates.
     """
-    # Consider the condition where x size and y size are different
+
+    #Fail saves
+    #If given 0 or negative # of cities, return empty list
+    if n_cities <= 0: 
+        return []
+
+    #If given 0 or negative size for map, return empty list
+    if size[0] <= 0:
+        if size[1] <= 0:
+            return []
+
+    #Create empty city list
+    citylist = []
+
+    #Coordinate of individual city in pair (x,y)
+    citycoord = (0,0)
+
+    #Constructing city list
+    i = 1
+    while i <= n_cities:
+        #Generate random coord within map size for city
+        #Consider the condition where x size and y size are different
+        citycoord = (random.randint(0,size[0]-1), random.randint(0,size[1]-1))
+
+        #Add city to list after coord is set
+        citylist.append(citycoord)
+
+        #Next iteration
+        i=i+1
+
+    #Return completed city list
+    return citylist
+
     pass
 
 def get_routes(city_names):
@@ -29,6 +61,16 @@ def get_routes(city_names):
     :return: A list of tuples representing all possible links between cities/ pairs of cities, 
             each item in the list (a link) represents a route between two cities.
     """
+
+    #Create empty route list
+    routelist = []
+
+    #Generate all posible connections between city names using itertools.combinations, store all posibilities to route list
+    routelist = itertools.combinations(city_names,2)
+
+    #Return completed route list
+    return routelist
+
     pass
 
 
