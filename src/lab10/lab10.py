28349de38546196cf9ad18f58cede2f587bd7cd1
Wen <jbw5872@psu.edu>
2023-03-17 09:31:11 -0400

    Lab 10 complete

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -10,12 +10,13 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
 string_col = data.select_dtypes(include="object").columns
 df = pd.get_dummies(data, columns=string_col, drop_first=False)
+print("")
 print(data.head())
 
 y = df.HeartDisease.values
@@ -26,12 +27,29 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
-sklearn_model = None
+sklearn_model = KNeighborsClassifier(n_neighbors=2)
+sklearn_model = sklearn_model.fit(x_train,y_train)
+
+# For debug purpose
+#print(x_test)
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
 
 """ Improve the model by normalizing the input data. """
+
+df_nor= (df-df.min()) / (df.max()-df.min())
+
+y = df_nor.HeartDisease.values
+x = df_nor.drop(["HeartDisease"], axis=1)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=25
+)
+
+# For debug purpose
+#print(x_test)
+
+sklearn_model = sklearn_model.fit(x_train, y_train)
 
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
