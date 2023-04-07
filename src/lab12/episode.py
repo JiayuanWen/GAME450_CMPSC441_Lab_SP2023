01deb13cc1c16515ceac43848b435e826bd2c2d1
JiayuanWen <jbw5872@psu.edu>
2023-04-07 03:18:11 -0400

    Lab 12 complete... somewhay

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -11,3 +11,44 @@ Note that observation/state is a tuple of the form (player1_health, player2_heal
 Action is simply the weapon selected by the player.
 Reward is the reward for the player for that turn.
 '''
+import sys
+from pathlib import Path
+
+sys.path.append(str((Path(__file__) / ".." / "..").resolve().absolute()))
+
+from lab11.turn_combat import Combat
+
+def run_episode(player1, player2):
+    episode_log = []
+
+    currentGame = Combat()
+
+    battle_ground = [player1, player2]
+
+    while not currentGame.gameOver:
+
+        states_player1 = list(reversed([(player1.health, player1.weapon) for player1 in battle_ground]))
+        states_player2 = list(reversed([(player2.health, player2.weapon) for player2 in battle_ground]))
+
+        for player1, state in zip(battle_ground, states_player1):
+            player1.selectAction(state)
+
+        for player2, state in zip(battle_ground, states_player2):
+            player2.selectAction(state)
+
+        currentGame.newRound()
+        currentGame.takeTurn(player1, player2)
+
+        print("%s's health = %d" % (player1.name, player1.health))
+        print("%s's health = %d" % (player2.name, player2.health))
+
+        observation = (player1.health, player2.health)
+        action = (player1.weapon, player2.weapon)
+        reward = currentGame.checkWin(player1, player2)
+
+        episode_log.append((observation, action, reward))
+
+    return episode_log
+
+
+        
